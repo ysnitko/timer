7471328d6ae876f9d3c4844c18a20cabe9fe77ec
yuri_sn <yuri.snitko@gmail.com>
2023-04-15 16:25:28 +0300

    js task

--- script/script.js
+++ script/script.js
@@ -10,34 +10,40 @@ const setTime = document.querySelector('.set-time');
 const addTime = document.querySelector('.add-time');
 
 function start() {
+  if (intervalId) {
+    return;
+  }
   intervalId = setInterval(() => {
-    milliseconds += 50;
-    if (milliseconds === 1000 ) {
-      milliseconds = 0;
-      time++
+    if (milliseconds === 0 && time === 0) {
+      clearInterval(intervalId);
+      return;
     }
-    timer () 
+    if (milliseconds === 0) {
+      time--;
+      milliseconds = 1000;
+    }
+    milliseconds -= 50;
+    timer();
   }, 50);
 }
 
-function timer () {
+function timer() {
   const hours = Math.floor(time / 3600)
-  .toString()
-  .padStart(2, '0');
-const minutes = Math.floor(time / 60 % 60)
-  .toString()
-  .padStart(2, '0');
-const seconds = Math.floor(time % 60)
-  .toString()
-  .padStart(2, '0');
-const mili =  milliseconds
     .toString()
-    .padStart(2, '0').slice(0, 2);
-addTimer.textContent = `${hours}:${minutes}:${seconds}.${mili}`
+    .padStart(2, '0');
+  const minutes = Math.floor((time / 60) % 60)
+    .toString()
+    .padStart(2, '0');
+  const seconds = Math.floor(time % 60)
+    .toString()
+    .padStart(2, '0');
+  const mili = milliseconds.toString().padStart(2, '0').slice(0, 2);
+  addTimer.textContent = `${hours}:${minutes}:${seconds}.${mili}`;
 }
 
 function stop() {
   clearInterval(intervalId);
+  intervalId = 0;
 }
 
 function reset() {
@@ -45,6 +51,7 @@ function reset() {
   time = 0;
   milliseconds = 0;
   addTimer.textContent = '00:00:00.00';
+  intervalId = 0;
 }
 
 function onClickBtn(event) {
@@ -72,29 +79,23 @@ function setTimeValue(event) {
     return;
   }
   let timeValue = +target.dataset.time;
-  console.log(typeof timeValue);
-  let setMinutes = (timeValue/60).toString().padStart(2, '0');
+  let setMinutes = (timeValue / 60).toString().padStart(2, '0');
   reset();
   time = timeValue;
-  addTimer.textContent = `00:${setMinutes}:00.00`
+  addTimer.textContent = `00:${setMinutes}:00.00`;
 }
 
 function addTimeValue(event) {
   let target = event.target;
-  console.log(target);
   if (target.tagName !== 'LI') {
     return;
   }
   let timeValue = +target.dataset.time;
   stop();
   time += timeValue;
-  timer()
+  timer();
 }
 
 setTime.addEventListener('click', setTimeValue);
 addTime.addEventListener('click', addTimeValue);
 btnContainer.addEventListener('click', onClickBtn);
-
-
-
-
